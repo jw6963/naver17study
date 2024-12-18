7272eefffc0e65f1f473da422c2b0d6e23e920da
jongwon-jeon <jw6963@naver.com>
2024-12-18 19:30:28 +0900

    1218

---
+++ javawork/NaverJavaProject/src/day1218/Book217.java
@@ -0,0 +1,15 @@
+package day1218;
+
+public class Book217 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car myCar = new Car();
+		System.out.println("모델명 : "+myCar.model);
+		System.out.println("시동 여부: "+myCar.start);
+		System.out.println("현재 속도: "+myCar.speed);
+		
+	}
+
+}

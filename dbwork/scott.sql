b3f080b2c18cdd8963fe4cd845e0b09cd779798c
jongwon-jeon <jw6963@naver.com>
2024-12-30 17:31:53 +0900

    1230

--- dbwork/scott.sql
+++ dbwork/scott.sql
@@ -62,7 +62,7 @@ select * from emp where job in ('MANAGER','SALESMAN','ANALYST'); -- 방법2
 select * from emp where empno in (7654,7788,7902);
 
 -- comm이 null인 데이터만 조회
-select * from emp where comm is null;
+select * from emp where comm is null; 
 
 -- comm이 null이 아닌 데이터만 조회
 select * from emp where comm is not null;
@@ -71,8 +71,103 @@ select * from emp where comm is not null;
 select * from emp where mgr is not null;
 
 -- comm이 null 인 경우는 0으로 출력, mgr은 null인 경우 100으로 출력
-select ename, job, sal, nvl(mgr,100) as mgr, nvl(comm,0) as comm from emp;
+select ename, job, sal, nvl(mgr,100) as mgr, nvl(comm,0) as comm from emp; -- as 생략 가능
 
 select sal, comm, sal+comm from emp; -- comm이 null일 경우 sal+comm도 null
 -- 위의 경우 sal+comm 이 경우 comm이 null이면 0으로 계산
 select sal,comm,sal+nvl(comm,0) from emp;
+
+-- 총 레코드 갯수
+select (count(*)) from emp;
+select count(*) count from emp; -- 이경우는 컬럼명이 count
+select count(*) 총갯수 from emp; -- 이경우는 컬럼명이 총갯수
+
+-- 문자열을 컬럼에 추가시 || 연산자 사용
+select '내 이름은 '||ename||'입니다' 자기소개 from emp;
+select '내 직업은 '||job||'이고, 내 월 급여는 '||sal||'입니다.' 자기소개 from emp;
+
+-- sal이 1500~2000 사이가 아닌 경우만조회
+select * from emp where sal not between 1500 and 2000;
+
+-- not in 을 사용하여 job이 salesman, clerk가 아닌 경우만 조회
+select * from emp where job not in ('SALESMAN', 'CLERK');
+
+-- GROUP 함수(COUNT,SUM,AVG,MAX,MIN
+select count(*) FROM EMP;
+select sum(sal) from emp;
+select avg(sal) from emp;
+select round(avg(sal),2) from emp;
+select max(sal) from emp;
+select min(sal) from emp;
+
+-- 평균급여보다 sal이 더 높은 사람을 조회하시오.
+select ename,sal from emp where sal > (select avg(sal) from emp);
+
+-- scott의 직업과 같은 직업을 가진 사람을 조회하시오.
+select ename, job from emp where job = (select job from emp where ename='SCOTT');
+
+-- scott의 mgr과 같은 mgr을 가진 사람
+select ename, mgr from emp where mgr = (select mgr from emp where ename='SCOTT');
+
+-- group by
+select job 직업,count(*) 인원수 from emp group by job;
+
+-- job의 group 별로 인원수를 구해보자(인원이 3명 이상인 경우에만 조회)
+select job 직업, count(*) 인원수 from emp group by job having count(job)>=3;
+
+-- job의 group 별로 인원수를 구해보자(인원이 3명 이상인 경우에만 조회) : 인원순으로 오름차순
+select job 직업, count(*) 인원수 from emp group by job having count(job)>=3 order by 인원수;
+
+-- 직업별 인원수, 최저연봉, 최고연봉, 평균연봉(소숫점이하 없이)을 구하시오.
+select job 직업, count(*) 인원수, min(sal) 최저연봉, max(sal) 최고연봉, round(avg(sal),0) 평균연봉 from emp group by job;
+
+-- 숫자 함수
+select abs(-5),abs(5) from dual;
+
+-- round : 반올림, ceil : 올림, floor: 내림
+select round(3.6,0), ceil(3.6), floor(3.6) from dual; // 4,4,3
+select round(3.4,0), ceil(3.4), floor(3.4) from dual; // 3,4,3
+select round(328947,-2) from dual; // 328900 
+select round(328467,-2) from dual; // 328500
+
+-- power(M,N) : M의 N승 값을 구함. mod(M,N) : M을 N으로 나눈 나머지
+select power(3,3), mod(10,3) from dual; // 27, 1
+
+-- 문자함수 concat(두 문자 더하기), lower(소문자), upper(대문자), initcap(첫글자만 대문자)
+select ename, concat(ename, '님'), lower(ename), upper(ename), initcap(ename) from emp;
+
+select LPAD(sal,10,'*') from emp; -- 총 10자리 중 남는 부분을 왼쪽부터 *로 채움
+select RPAD(sal,10,'*') from emp; -- 총 10자리 중 남는 부분을 오른쪽부터 *로 채움
+
+select substr('HAPPY DAY', 7, 3) from dual; -- 7번째 글자부터 3글자 추출
+select substr('HAPPY DAY', -6, 3) from dual; -- 뒤에서 6번째 글자부터 3글자 추출
+
+-- emp 테이블의 ename에서 왼쪽 3글자를 추출 후 총 7자리 중 우측 빈공간은 *로 채워서 출력
+select rpad(substr(ename, 1,3),7,'*') from emp;
+
+-- length 길이 구하기
+select ename 이름, length(ename) from emp;
+
+-- replace - happy를 good 으로 변경
+select replace('HAPPY DAY', 'HAPPY','GOOD') from dual;
+
+-- trim : 앞 뒤 공백 제거
+select '*'||'    Lee su ji   '||'*' from dual;
+select '*'||trim('    Lee su ji   ')||'*' from dual;
+
+-- 일단 현재 날짜를 나타내는 sysdate
+select sysdate from dual; -- 현재 날짜
+select sysdate+7 from dual; -- 오늘로부터 7일 뒤 날짜
+select sysdate+1 from dual; -- 내일 날짜
+
+-- to_char 함수를 이용해서 원하는 날짜 모양으로 출력해보자
+select to_char(sysdate, 'yyyy-mm-dd') from dual; 
+select to_char(sysdate, 'yyyy-mm-dd am hh:mi') from dual; -- 2024-12-30 오후 04:44
+select to_char(sysdate, 'yyyy-mm-dd hh24:mi') from dual; -- 2024-12-30 16:44
+select to_char(sysdate, 'month') from dual; -- (지역화에 따라 다르게 나오므로 잘 사용 안 함)
+
+-- 현재 년도 4자리만 추출
+select to_char(sysdate, 'yyyy') from dual;
+-- 현재 월 추출
+select to_char(sysdate, 'mm') from dual;
+select to_char(to_date('2024-5-10'),'mm') from dual; -- 05
